9be29038b78f6b560b23baedb89db75a1ee564d3
kapilagrawal <kapilagrawal.408@gmail.com>
2022-06-05 16:59:18 +0530

    changes add file

--- Add.java
+++ Add.java
@@ -2,6 +2,7 @@ class A{
 	public static void main(String A.java){
 		int a = 10;
 		int b = 20;
-		System.out.println("the addition of " + a + " and " + b + " is " + (a + b))
+		System.out.println("the addition of " + a + " and " + b + " is " + (a + b));
+		System.out.println("All is well");
 	}
 }
