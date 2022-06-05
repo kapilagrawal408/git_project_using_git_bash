4cf7d28806b78301c83adb5d807f33826183c385
kapilagrawal <kapilagrawal.408@gmail.com>
2022-06-05 17:04:31 +0530

    Multiply file added

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+class Multiply{
+	public static void main(String args[]){
+		int a = 20;
+		int b = 10;
+		System.out.println("The multiplication of " + a + " to the " + b + " is " + (a * b));
+	}
+}
